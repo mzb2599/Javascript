f3a2be9917d04eb09af944dcb3478c9d20f02613
mzaki2599 <mzaki2599@gmail.com>
2025-04-18 20:38:09 +0530

    if else example

---
+++ Javascript_Study_Month/week1/conditionalStatements.js
@@ -0,0 +1,21 @@
+// if else example
+
+let age = 18;
+
+// if else example
+// The if statement evaluates a condition and executes the block of code inside it if the condition is true.
+if (age > 18) {
+    console.log("You are an adult.");
+}
+// The else if statement allows you to check multiple conditions. If the first condition is false, it checks the next condition.
+else if (age < 18) {
+    console.log("You are a minor.");
+}
+else if (age === 18) {
+    console.log("You just became an adult.");
+}
+// The else statement executes a block of code if none of the previous conditions are true.
+else {
+    console.log("You are not an adult yet.");
+}
+
