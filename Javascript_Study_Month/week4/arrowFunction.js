04afa62bc9eacb16177634275ba15d2fa034767f
mzaki2599 <mzaki2599@gmail.com>
2025-09-17 21:22:16 +0530

    Arrow function

---
+++ Javascript_Study_Month/week4/arrowFunction.js
@@ -0,0 +1,11 @@
+// Arrow function in js
+const add = (a, b) => a + b;
+console.log(add(2, 3)); // Output: 5
+// Arrow function with single parameter
+const square = x => x * x;
+
+console.log(square(4)); // Output: 16
+
+// Arrow function with no parameters
+const greet = () => console.log("Hello, World!");
+greet(); // Output: Hello, World!
